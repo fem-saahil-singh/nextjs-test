494c77693cea4e70fef31ade9946922918f8ce95
fem-saahil-singh <93595504+fem-saahil-singh@users.noreply.github.com>
2022-05-23 20:40:00 +0530

    added awards

---
+++ pages/awards.tsx
@@ -0,0 +1,15 @@
+import Head from "next/head";
+
+function Awards() {
+  return (
+    <>
+      <Head>
+        <title>Awards - Swapnadeep</title>
+        <meta name="description" content="Awards won by Swapnadeep Mohapatra" />
+      </Head>
+      <h1>Awards</h1>
+    </>
+  );
+}
+
+export default Awards;
